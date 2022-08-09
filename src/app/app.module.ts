d150babee14548b5f76334ed2c669b404cf41ddf
Öğünç Filiz <ogunc784@gmail.com>
2022-08-09 16:50:18 +0300

    PrimeNG download

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
+import {MenuItem} from 'primeng/api';                  //api
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AccordionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
